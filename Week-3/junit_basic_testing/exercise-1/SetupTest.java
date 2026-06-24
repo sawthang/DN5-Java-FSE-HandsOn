5fe35f7e9fc5b1f279326056045271a530d539c3
sawthang <59901720+sawthang@users.noreply.github.com>
2026-06-25 02:00:48 +0530

    Add files via upload

---
+++ Week-3/junit_basic_testing/exercise-1/SetupTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SetupTest {
+
+    @Test
+    public void testJUnitSetup() {
+        assertEquals(2, 1 + 1);
+    }
+}
