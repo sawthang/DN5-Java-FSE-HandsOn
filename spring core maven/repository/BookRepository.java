ec54ab19df96fc301cdba0d1e03bd0d3897e1de2
sawthang <59901720+sawthang@users.noreply.github.com>
2026-07-01 11:30:47 +0530

    Delete spring core maven directory

--- spring core maven/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-
-    public void displayBook() {
-        System.out.println("BookRepository: Book data retrieved successfully.");
-    }
-
-}
