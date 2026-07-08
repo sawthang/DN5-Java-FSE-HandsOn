704359fd1ca6999305c63302bf6a65090a0533f2
sawthang <59901720+sawthang@users.noreply.github.com>
2026-07-09 01:50:31 +0530

    Delete Spring Data JPA - Quick Example/orm-learn/orm-learn directory

--- Spring Data JPA - Quick Example/orm-learn/orm-learn/sql/country.sql
+++
@@ -1,8 +0,0 @@
-create schema ormlearn;
-
-use ormlearn;
-
-create table country(co_code varchar(2) primary key, co_name varchar(50));
-
-insert into country values ('IN', 'India');
-insert into country values ('US', 'United States of America');
